d5f27bc48855cfb2f9a81dd6cfce6cc2956dbc7b
Konstantin Chernykh <costaslavich@gmail.com>
2019-08-04 00:26:09 +0300

    install again gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import App from './Components/App';
-import './styles.css';
+import './index.css';
 
 ReactDOM.render(
   <HashRouter basename="/">
